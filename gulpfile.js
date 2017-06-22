ef5bbd6a4b23dbbaf539b9488a49df5039da7d67
Clayton Duarte <webdesign@claytonduarte.com>
2017-06-22 17:42:16 -0300

    :ant: gulp+sass+pug

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+//SETUP
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+    postcss = require('postcss-scss');
+    pug = require('gulp-pug');
+    browserSync = require('browser-sync').create();
+
+
+//TASKS
+
+gulp.task('sass', function() {
+     return gulp.src('src/flexgh.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('src/*.pug')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass', 'pug'], function () {
+    browserSync.init({
+        server: "dist"
+    });
+    gulp.watch("src/*.scss", ['sass']);
+    gulp.watch("src/*.pug", ['pug']);
+    gulp.watch("dist/*.html")
+        .on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
